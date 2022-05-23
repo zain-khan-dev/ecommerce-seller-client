7c8630fa11c1ae907b75f7082e7c2a2342646a21
Zain Khan <zainkhanjune@gmail.com>
2022-05-23 09:24:51 +0530

    Updated create product to include category

--- src/component/CreateProduct.tsx
+++ src/component/CreateProduct.tsx
@@ -13,14 +13,16 @@ const CreateProduct = () => {
                 name:'',
                 description:'',
                 price: '',
-                stock: ''
+                stock: '',
+                category:''
             }}
             onSubmit={(values)=>{
                 var regexp = /^\d+\.\d{0,2}$/; // regex to match decimal
                 console.log(values.price)
+                console.log(values.category)
                 if(regexp.test(values.price)){
                     const localPrice = parseFloat(values.price)
-                    postAuthData("product/", {name:values.name, description:values.description,stock:values.stock,price:values.price})
+                    postAuthData("product/", {name:values.name, description:values.description,stock:values.stock,price:values.price, category:values.category})
                     .then((result)=>console.log(result))
                     .catch((e)=>alert("Difficult" + e))
                 }
@@ -60,6 +62,42 @@ const CreateProduct = () => {
                         id="stock"
                         placeholder="Enter the product stock"
                     /> 
+                    <label className="mt-4 text-xl font-medium">Category</label>
+                    <Field 
+                        as="select" 
+                        name="category"
+                        id="category"
+                        className="bg-white w-full px-3 py-2 rounded-xl shadow-xl font-sans">
+                        <option value='Appli'>Appliances</option>
+                        <option value='Apps '>Apps & Games</option>
+                        <option value='Arts,'>Arts, Crafts, & Sewing</option>
+                        <option value='Autom'>Automotive Parts & Accessories</option>
+                        <option value='Baby'>Baby</option>
+                        <option value='Beaut'>Beauty & Personal Care</option>
+                        <option value='Books'>Books</option>
+                        <option value='CDs &'>CDs & Vinyl</option>
+                        <option value='Cell '>Cell Phones & Accessories</option>
+                        <option value='Cloth'>Clothing, Shoes and Jewelry</option>
+                        <option value='Colle'>Collectibles & Fine Art</option>
+                        <option value='Compu'>Computers</option>
+                        <option value='Elect'>Electronics</option>
+                        <option value='Garde'>Garden & Outdoor</option>
+                        <option value='Groce'>Grocery & Gourmet Food</option>
+                        <option value='Handm'>Handmade</option>
+                        <option value='Healt'>Health, Household & Baby Care</option>
+                        <option value='Home '>Home & Kitchen</option>
+                        <option value='Indus'>Industrial & Scientific</option>
+                        <option value='Lugga'>Luggage & Travel Gear</option>
+                        <option value='Movie'>Movies & TV</option>
+                        <option value='Music'>Musical Instruments</option>
+                        <option value='Offic'>Office Products</option>
+                        <option value='Pet S'>Pet Supplies</option>
+                        <option value='Sport'>Sports & Outdoors</option>
+                        <option value='Tools'>Tools & Home Improvement</option>
+                        <option value='Toys '>Toys & Games</option>
+                        <option value='Video'>Video Games</option>
+
+                    </Field>
                     <button className="mt-4 bg-green-600 px-3 py-2 rounded-xl" type="submit">Create</button>
                 </Form>
             </Formik>
